755a6c1dacd4c1324d180e728dcc75481fb573f1
lel020 <mahmud.jawaad03@gmail.com>
2023-05-28 14:02:02 +1000

    Javascript code now loads

---
+++ src/script.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+
+    //For the links-container (home, about, services, contact)
+    const homeButton = document.getElementById('home');
+    const servicesButton = document.getElementById('services');
+
+    homeButton.addEventListener('click', () => {
+    document.getElementById('header-main').scrollIntoView({ behavior: 'smooth'});
+    });
+
+
+
+    //For the login and register buttons under "login-container"
+    const loginButton = document.getElementById('login');
+    const registerButton = document.getElementById('register');
+
+    loginButton.addEventListener('click', () => {
+        window.open('');
+    }); 
+    registerButton.addEventListener('click', () => {
+        window.open('');
+    }); 
+
+
+};
